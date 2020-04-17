26fc35072bf93054270c2818c22ae63ec4548e36
Kai Roth <info@rothedv.com>
2020-04-17 17:56:40 +0200

    Added viewport recognition V1

--- js/app.js
+++ js/app.js
@@ -17,8 +17,51 @@
 // Define Global Variables
 const sectionList = document.getElementsByTagName('section');
 const navbar = document.getElementById('navbar__list');
+const section1 = document.getElementById('section1');
+const section2 = document.getElementById('section2');
+const section3 = document.getElementById('section3');
+const section4 = document.getElementById('section4');
+
+
 console.log('Navbar ' + navbar.id);
 
+document.addEventListener('click', function(){
+const navbarHeight = navbar.offsetHeight;
+const screenHeight = window.innerHeight;
+// console.log('Navbar Height:' + navbarHeight  + 'px');
+console.log(window.innerHeight);
+if(((section1.getBoundingClientRect().y + navbarHeight)>0) & ((section1.getBoundingClientRect().y + navbarHeight)<screenHeight)){
+    console.log('Section1 is currently viewed');
+}
+else  console.log('Section1 outside of view');
+
+if(((section2.getBoundingClientRect().y + navbarHeight)>0) & ((section2.getBoundingClientRect().y + navbarHeight)<screenHeight)){
+    console.log('Section2 is currently viewed');
+}
+else  console.log('Section2 outside of view');
+
+if(((section3.getBoundingClientRect().y + navbarHeight)>0) & ((section3.getBoundingClientRect().y + navbarHeight)<screenHeight)){
+    console.log('Section3 is currently viewed');
+}
+else  console.log('Section3 outside of view');
+
+if(((section4.getBoundingClientRect().y + navbarHeight)>0) & ((section4.getBoundingClientRect().y + navbarHeight)<screenHeight)){
+    console.log('Section4 is currently viewed');
+}
+else  console.log('Section4 outside of view');
+
+// console.log('Section1 Top Position: ' + section1.getBoundingClientRect().y);
+// console.log('Section1 Bottom Position: ' + document.getElementById('section1').getBoundingClientRect().bottom);
+// console.log('Section2 Top Position: ' + document.getElementById('section2').getBoundingClientRect().y);
+// console.log('Section2 Bottom Position: ' + document.getElementById('section2').getBoundingClientRect().bottom);
+// console.log('Section3 Top Position: ' + document.getElementById('section3').getBoundingClientRect().y);
+// console.log('Section3 Bottom Position: ' + document.getElementById('section3').getBoundingClientRect().bottom);
+// console.log('Section4 Top Position: ' + document.getElementById('section4').getBoundingClientRect().y);
+// console.log('Section4 Bottom Position: ' + document.getElementById('section4').getBoundingClientRect().bottom);
+});
+
+
+document.getElementById('section1');
 /**
  * End Global Variables
  * Start Helper Functions
@@ -65,18 +108,14 @@ console.log('Navbar ' + navbar.id);
 
 
 for (let sec of sectionList){
-    // function scrollToTarget(){
-    //     sec.scrollIntoView({ block: 'start',  behavior: 'smooth' });
-    // };
     console.log('section Id ' + sec.id);
+    //  Populating the "heading" constant with the Text content of the collected heading  
     const heading = sec.querySelector('h2').textContent;
+    //  Debug heading content   
     console.log('heading ' + heading);
+    // Creating a new list item and storeit  in the constant "item"
     const item = document.createElement('li');
-    // item.innerHTML = "<a href='#'>"+heading+"</a>";
-    // item.textContent = heading;
-    // item.innerHTML = "<a href='#"+sec.id+"'>"+heading+"</a>";
     item.textContent = heading;
-    // item.addEventListener('click', scrollToTarget());
     navbar.appendChild(item);
     item.addEventListener('click', function(){
         sec.scrollIntoView({ block: 'start',  behavior: 'smooth' });
@@ -84,17 +123,6 @@ for (let sec of sectionList){
     
 }
 
-// const child = document.getElementById('navbar__list').firstChild;
-// child.textContent;
-// child.child
-
-// const listLi = navbar.getElementsByTagName('li');
-// console.log(listLi[0].innerText);
-// function lorem(){};
-// function testFunc(){
-//     listLi[0].style.color = 'red';
-// };
-// listLi[0].addEventListener('dbclick',testFunc());
 
 
 
